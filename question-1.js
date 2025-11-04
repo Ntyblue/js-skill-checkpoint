a63e682d5995404a9129dcc617ab6658954da4be
namtan <sirinrat.laoprom@gmail.com>
2025-11-04 23:41:28 +0700

    do question 1 and 2

--- question-1.js
+++ question-1.js
@@ -6,3 +6,24 @@ const inventory = {
 };
 
 // เริ่มเขียนโค้ดตรงนี้
+inventory.apple.quantity = 200;
+inventory.orange = { price: 20, quantity: 300 };
+/*
+ลอง
+inventory.orange = {};
+inventory.orange.price = 20;
+inventory.orange.quantity = 300;
+console.log(inventory);
+*/
+
+let totalPrice = 0;
+for (const fruit in inventory) {
+  /*
+  console.log(fruit);
+  console.log(inventory[fruit]);
+  console.log(inventory[fruit].price);
+  */
+  totalPrice += inventory[fruit].price;
+}
+
+console.log(`Total inventory value: ${totalPrice} baht`);
