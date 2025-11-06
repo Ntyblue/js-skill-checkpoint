2d926da0b4033931f14ae92b87935d1ef5a2bd7b
namtan <sirinrat.laoprom@gmail.com>
2025-11-07 03:03:02 +0700

    do question 8

--- question-8.js
+++ question-8.js
@@ -1,3 +1,33 @@
 // Question #8: Fetching User List from Server
 
 // เริ่มเขียนโค้ดตรงนี้
+/*
+fetch("https://jsonplaceholder.typicode.com/users");
+console.log(fetch("https://jsonplaceholder.typicode.com/users"));
+fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+  response.json()
+);
+console.log(
+  fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+    response.json()
+  )
+);
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+fetch("https://jsonplaceholder.typicode.com/users")
+  .then((data) => data.json())
+  .then((users) => {
+    const newUsers = users.map((user) => user.name);
+    console.log(newUsers);
+  });
+*/
+
+async function getUsersName() {
+  const getUsers = await fetch("https://jsonplaceholder.typicode.com/users");
+  const transformUsers = await getUsers.json();
+  const newUsers = await transformUsers.map((users) => users.name);
+  console.log(newUsers);
+}
+
+getUsersName();
